d81a7122ca94f7a12d21c5a6c0da4074430554b8
vairakkani <vairakkani@gmail.com>
2019-12-13 09:16:52 +0530

    adding static files

--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
